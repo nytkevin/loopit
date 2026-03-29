b3d5a6bf00352b7a032424329ffb1b8295dcfbcf
nytkevin <nzayiturikikevin@gmail.com>
2026-03-29 14:56:57 -0400

    fix: handling error and loading from movies,tvshows

--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -12,7 +12,7 @@ export default function TopRatedMovies() {
   const searchParams = useSearchParams();
   const page = Number(searchParams.get("page")) || 1;
 
-  const { isPending, error, data } = useQuery({
+  const { isLoading, error, data } = useQuery({
     queryKey: ["movies", page],
     queryFn: () =>
       fetch(
@@ -20,8 +20,26 @@ export default function TopRatedMovies() {
         options,
       ).then((res) => res.json()),
   });
-  if (isPending) return "Loading...";
-  if (error) return "An error has occurred: " + error.message;
+
+  if (isLoading) {
+    return (
+      <section className="grid grid-cols-2 gap-5 mx-5 md:grid-cols-6 md:gap-8">
+        {Array.from({ length: 12 }).map((_, index) => (
+          <div
+            key={index}
+            className="bg-gray-800 animate-pulse h-64 rounded-md"
+          />
+        ))}
+      </section>
+    );
+  }
+
+  if (error)
+    return (
+      <div className="text-red-500">
+        An error occurred: {(error as Error).message}
+      </div>
+    );
 
   return (
     <>
--- app/tv-shows/page.tsx
+++ app/tv-shows/page.tsx
@@ -18,9 +18,25 @@ export default function Tvshows() {
         options,
       ).then((res) => res.json()),
   });
+  if (isLoading) {
+    return (
+      <section className="grid grid-cols-2 gap-5 mx-5 md:grid-cols-6 md:gap-8">
+        {Array.from({ length: 12 }).map((_, index) => (
+          <div
+            key={index}
+            className="bg-gray-800 animate-pulse h-64 rounded-md"
+          />
+        ))}
+      </section>
+    );
+  }
 
-  if (isLoading) return "Loading...";
-  if (error) return "An error has occurred: " + error.message;
+  if (error)
+    return (
+      <div className="text-red-500">
+        An error occurred: {(error as Error).message}
+      </div>
+    );
   return (
     <section className="grid grid-cols-2 gap-5 mx-5 md:grid-cols-6 md:px-40 md:gap-10 pb-10">
       {data.results.map((tvshow: Movies) => (
